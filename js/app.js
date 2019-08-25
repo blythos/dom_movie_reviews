71cd1e9a013f6a33418f7950885459a84273329e
Stephen Blythe <stephenblythe@gmail.com>
2019-08-25 13:25:14 +0100

    Adds an array of movie genres

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
-})
+  console.log('JavaScript loaded');
+
+  initialGenreList = document.querySelector('#genre');
+  createGenreList();
+
+  // const movieForm = document.querySelector('#new-item-form');
+  // movieForm.addEventListener('submit', addNewMovie);
+  //
+  // const deleteAllButton = document.querySelector('#delete-all-button');
+  // deleteButton.addEventListener('submit', deleteAllMovies);
+
+});
+
+reviewsList = [];
+
+genreList = ["Horror", "Sci-Fi", "Action"];
+
+const createGenreList = function() {
+  for (let genre of genreList) {
+    let option = document.createElement("option");
+    option.value = genre;
+    option.text = genre;
+    initialGenreList.appendChild(option);
+    console.log(this);
+  };
+};
